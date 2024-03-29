df7e4a12eeab6763299b05a42ab47344573f2b93
JanBash <158750247+JanBash@users.noreply.github.com>
2024-03-29 13:37:29 +0600

    Comments settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sakirovzanbolot48@gmail.com'
-EMAIL_HOST_PASSWORD = 'kgjb eswr wqul gaer'
+EMAIL_HOST_USER = 'sakirovzanbolot48@gmail.com' # <-- You can change email instance here
+EMAIL_HOST_PASSWORD = 'kgjb eswr wqul gaer' # <-- Code from Google
