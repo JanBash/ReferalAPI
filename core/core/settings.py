8908a46e2622a6c39aa8be3dc86860be9075d386
JanBash <158750247+JanBash@users.noreply.github.com>
2024-03-31 01:38:10 +0600

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Swagger
+    'drf_yasg',
     
     # DRF Apps
     'rest_framework',
