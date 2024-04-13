826459ae1991863bf3a23d5cfbea587f7c80b4af
JanBash <jan13_2018.29@mail.ru>
2024-04-13 17:18:08 +0600

    psql

--- core/core/settings.py
+++ core/core/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'refer',  # Имя вашей базы данных
+        'USER': 'janbash',      # Имя вашего пользователя
+        'PASSWORD': '152020',  # Ваш пароль
+        'HOST': 'localhost',   # Хост, на котором работает PostgreSQL
+        'PORT': '5432',            # Порт (по умолчанию 5432)
     }
 }
 
